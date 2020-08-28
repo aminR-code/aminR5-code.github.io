8b006c6955ff280f710e5d417844ad25788331a2
aminR-code <amin.rekto21@gmail.com>
2020-08-28 13:57:04 +0700

    amin

---
+++ script.js
@@ -0,0 +1,32 @@
+function RollMe () {
+    let output = 0
+
+    let randomOne = Math.floor(Math.random()*6+1)
+    let randomImageOne = 'images/' + randomOne + '.png'
+
+    // console.log(randomOne)
+    // console.log(randomImageOne)
+
+    let randomTwo = Math.floor(Math.random()*6+1)
+    let randomImageTwo = 'images/' + randomTwo + '.png'
+
+    let imgOne = document.querySelectorAll('img') [0]
+    imgOne.setAttribute('src', randomImageOne)
+    
+    
+    let imgTwo = document.querySelectorAll('img') [1]
+    imgTwo.setAttribute('src', randomImageTwo)
+
+    // if(randomOne > randomTwo) {
+    //     document.querySelector('h1').innerHTML="YOU WIN"
+    // }
+    
+    // else if (randomOne < randomTwo) {
+    //     document.querySelector('h1').innerHTML="CPU WIN"
+    // }
+
+    
+    // else(randomOne > randomTwo) {
+    //     document.querySelector('h1').innerHTML="DRAW"
+    // }
+}
